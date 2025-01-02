cdc2b752a29435704bb7f1a63e6c2905737b292a
yurimoreiraa <yuri.mconsultoria@gmail.com>
2025-01-02 20:41:45 -0300

    atualização index.js

--- tests/support/index.js
+++ tests/support/index.js
@@ -7,13 +7,15 @@ const { Toast } = require('../pages/Components')
 
 const test = base.extend({
     page: async ({ page }, use) => {
-        await use({
-            ...page,
-            landing: new LandingPage(page),
-            login: new LoginPage(page),
-            movies: new MoviesPage(page),
-            toast: new Toast(page)
-        })
+        
+        const context = page
+
+        context['landing'] = new LandingPage(page)
+        context['login'] = new LoginPage(page)
+        context['movies'] = new MoviesPage(page)
+        context['toast'] = new Toast(page)
+
+        await use(page)
     }
 })
 
