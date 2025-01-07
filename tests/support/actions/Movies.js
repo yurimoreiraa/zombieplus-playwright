c4414f449cf54bcd5cb44f90f507fffd08cc1081
yurimoreiraa <yuri.mconsultoria@gmail.com>
2025-01-06 23:19:45 -0300

    atualização em movies.js e spec

--- tests/support/actions/Movies.js
+++ tests/support/actions/Movies.js
@@ -10,31 +10,38 @@ export class Movies {
         await this.page.locator('a[href$="register"]').click()
     }
 
-    async submit(){
+    async submit() {
         await this.page.getByRole('button', { name: 'Cadastrar' }).click()
 
     }
 
-    async create(title, overview, company, release_year) {
+    async create(movie) {
 
         await this.goForm()
 
-        await this.page.getByLabel('Titulo do filme').fill(title)
-        await this.page.getByLabel('Sinopse').fill(overview)
+        await this.page.getByLabel('Titulo do filme').fill(movie.title)
+        await this.page.getByLabel('Sinopse').fill(movie.overview)
 
         await this.page.locator('#select_company_id .react-select__indicator')
             .click()
 
         await this.page.locator('.react-select__option')
-            .filter({ hasText: company })
+            .filter({ hasText: movie.company })
             .click()
 
         await this.page.locator('#select_year .react-select__indicators')
             .click()
 
         await this.page.locator('.react-select__option')
-            .filter({ hasText: release_year })
+            .filter({ hasText: movie.release_year })
             .click()
+
+        await this.page.locator('input[name=cover]')
+            .setInputFiles('tests/support/fixtures' + movie.cover)
+
+        if (movie.featured) {
+            await this.page.locator('.featured .react-switch').click()
+        }
 
         await this.submit()
     }
